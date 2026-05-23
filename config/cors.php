15d8ea62087426738a1f14b23280846e62df820c
Louay Daher <louaydaher173@gmail.com>
2026-05-23 19:03:48 +0300

    changes

--- config/cors.php
+++ config/cors.php
@@ -1,19 +1,25 @@
 <?php
 
+$frontendOrigins = array_values(array_unique(array_filter(array_merge(
+    [
+        'http://localhost:8080',
+        'http://127.0.0.1:8080',
+    ],
+    array_map('trim', explode(',', (string) env('FRONTEND_URL', ''))),
+))));
+
 return [
 
     'paths' => ['api/*'],
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => array_filter([
-        env('FRONTEND_URL', 'http://localhost:8080'),
-        'http://localhost:8080',
-        'http://127.0.0.1:8080',
-    ]),
+    'allowed_origins' => $frontendOrigins,
 
     'allowed_origins_patterns' => [
         '#^http://192\.168\.\d{1,3}\.\d{1,3}:8080$#',
+        '#^http://localhost:\d+$#',
+        '#^http://127\.0\.0\.1:\d+$#',
     ],
 
     'allowed_headers' => ['*'],
